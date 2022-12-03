dbf774e9da6f57deb2c73b336d35e5071096dc3d
ghostd4t <abusygin076@gmail.com>
2022-12-03 12:23:25 +0300

    need to fix popping of list

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ typedef int Data;
 typedef struct Node {
     Data data;
     struct Node *next;
+    struct Node *prev;
+    
 } Node;
 
 void print(Node * list){
@@ -37,6 +39,12 @@ Data pop(Node ** plist){
     return res;
 }
 
+void killroot(Node* root){
+    Node* temp;
+    temp = root->next;
+    temp->prev = NULL;
+}
+
 
 int main(){
 
@@ -45,29 +53,73 @@ int main(){
     Node*min = NULL;
     int amount;
     Data var;
+    int max_element = -9999999;
 
+    printf("[+] Task 1:\n");
     printf("[+] Input amount of elements: ");
     scanf("%d", &amount);
     for(int i = 0; i < amount; i++){
         printf("[+] Element %d: ", i + 1);
         scanf("%d", &var);
-        if(var > 0){
-            push(&list, var);
-            push(&max, var);
-        }
-        else if(var < 0){
-            push(&list, var);
-            push(&min, var);
-        }else{
-            push(&list, var);
-        }
-        
+        push(&list, var);
     }
+    
+    for(Node * p = list; p != NULL; p = p -> next){
+        if(p -> data > 0){
+            push(&max, p -> data);
+        }else if(p -> data <0){
+            push(&min, p -> data);
+        }
+    }
+
     printf("[+] All elements:\n ");
     print(list);
     printf("[+] Elements \"> 0\":\n");
     print(max);
     printf("[+] Elements \"< 0\":\n");
     print(min);
+
+    for(Node * p = list; p != NULL; p = p -> next){
+        if (p -> data > max_element){
+            max_element = p -> data;
+        }
+    }
+
+   
+    printf("[+] Task 2:\n");
+    printf("[+] Max element: %d\n", max_element);
+    printf("[!] Erasing max elements...\n");
+    
+    
+    for(Node * p = list; p != NULL; p = p -> next){
+        if(p-> data == max_element){
+            if(p->prev == NULL){
+                killroot(p);
+            }else{
+                Data d = pop(&list);
+            }
+     
+
+        }
+        print(list);
+    }
+
+    /*   
+    for(Node * p = list; p != NULL; p = p -> next){
+        if(is_empty(list)){
+            return 0;
+        }else{
+            if (p -> data == max_element){
+            Data d = pop(&list);
+            print(list);
+            }
+        }
+        
+    }
+    */
+    print(list);
+    
+
+    
     return 0;
 }
